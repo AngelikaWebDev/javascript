27702e4b60b132eeb0863ea145899a99413f06ad
AngelikaWebDev <110480648+AngelikaWebDev@users.noreply.github.com>
2023-02-23 10:47:46 +0100

    Ejercicio Tema Sintaxis

---
+++ sintaxis/sintaxis.js
@@ -0,0 +1,21 @@
+/**Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+ * Tu nombre (string)
+ * Tu edad (number)
+ * ¿Eres desarrollador? (boolean)
+ * Tu fecha de nacimiento (Date)
+ * Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+ * 
+ */
+
+const lista = [
+  "Angelika",
+  29,
+  true,
+  new Date(1993, 05, 28),
+  { 
+      titulo: "Código Limpio",
+      autor: "Robert C.Martin",
+      fecha: new Date(2008, 01, 01),
+      url: "https://amzn.eu/d/bdoF9Aq"
+    },
+];
