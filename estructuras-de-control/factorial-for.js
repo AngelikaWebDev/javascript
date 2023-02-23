b5cc8f9ab0506585c8a76a630db4f9e9e15c9247
AngelikaWebDev <110480648+AngelikaWebDev@users.noreply.github.com>
2023-02-23 10:38:28 +0100

    Ejercicios Tema Estructuras de Control

---
+++ estructuras-de-control/factorial-for.js
@@ -0,0 +1,9 @@
+//Este archivo debe calcular el factorial de 10 utilizando un solo bucle for
+
+let factorial = 1;
+
+for(let i = 1; i <= 10; i++){
+    factorial *= 1;
+}
+
+console.log(factorial);
