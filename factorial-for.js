9a2ed0b273144a25f14be4a20c477b492ddf3a07
AngelikaWebDev <110480648+AngelikaWebDev@users.noreply.github.com>
2023-02-23 10:36:42 +0100

    Delete factorial-for.js

--- factorial-for.js
+++
@@ -1,9 +0,0 @@
-//Este archivo debe calcular el factorial de 10 utilizando un solo bucle for
-
-let factorial = 1;
-
-for(let i = 1; i <= 10; i++){
-    factorial *= 1;
-}
-
-console.log(factorial);
